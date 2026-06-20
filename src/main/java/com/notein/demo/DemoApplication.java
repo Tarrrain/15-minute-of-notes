8bb488800af519387cf7e56287b4b0ca887f72a9
antiquesik <lisanaeltonskaya@gmail.com>
2026-06-20 11:53:40 +0700

    Add Note entity, repository, service and controller

--- src/main/java/com/notein/demo/DemoApplication.java
+++ src/main/java/com/notein/demo/DemoApplication.java
@@ -3,29 +3,40 @@ package com.notein.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 @Entity
 public class DemoApplication {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO) //id создается автоматически
-	private long Id;
-
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //id создается автоматически
+	private Long Id;
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String content;
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+	public DemoApplication() {}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		tookAText();
+		//DemoApplication(content);
 	}
-	public static void tookAText()
+	public DemoApplication(String content)
 	{
+		this.content = content;
+		this.createdAt = LocalDateTime.now();
 		Scanner input = new Scanner(System.in);
 		System.out.print("notes:\n");
 		String text = input.nextLine();
 		System.out.println("you wrote:" + text);
 		return;
 	}
+	public Long getId() {return Id;}
+	public void setId(Long Id) {this.Id = Id;}
+	public String getContent() {return content;}
+	public void setContent(String content){this.content = content;}
+	public LocalDateTime getCreatedAt(){return createdAt;}
+	public void setCreatedAt(LocalDateTime createdAt){this.createdAt = createdAt;}
+
 
 }
